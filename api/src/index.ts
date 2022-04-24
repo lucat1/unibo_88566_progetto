eda86a664b04277b2ea2e2b8bbc90228d0ab6091
Luca <luca.tagliavini5@studio.unibo.it>
2022-04-24 16:06:12 +0200

    production api web server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,39 @@
-import polka, { Middleware } from "polka";
-import { API_PORT } from "../../endpoints.json";
+import polka from "polka";
+// @ts-ignore
+import send from "@polka/send-type";
+import sirv from "sirv";
+import { join } from "path";
+import { readFile } from "fs/promises";
 
-const one: Middleware = (req, _, next) => {
-  req.params = { ...req.params, hello: "world" };
-  next();
+import { API_PORT, API_ENDPOINT } from "../../endpoints.json";
+
+const sites = ["game", "frontoffice", "backoffice"],
+  app = polka();
+
+const main = async () => {
+  /* Serve all front sites in production */
+  if (API_ENDPOINT.includes("unibo.it")) {
+    for (const site of sites) {
+      const index = await readFile(
+        join(__dirname, "..", "..", site, "build", "index.html")
+      );
+      app
+        .use(`/${site}`, sirv(join(__dirname, "..", "..", site, "build")))
+        .get("*", (_, res) =>
+          send(res, 200, index, {
+            "Content-Type": "text/html",
+          })
+        );
+    }
+  }
+
+  app.get("/api/hello", (req, res) => {
+    res.end("hello world");
+  });
+
+  app.listen(API_PORT, () => {
+    console.log(`Running on localhost:${API_PORT}`);
+  });
 };
 
-polka()
-  .use(one)
-  .get("/users/:id", (req, res) => {
-    console.log(`~> Hello, ${req.params.hello}`);
-    res.end(`User: ${req.params.id}`);
-  })
-  .listen(API_PORT, () => {
-    console.log(`> Running on localhost:${API_PORT}`);
-  });
+main();
