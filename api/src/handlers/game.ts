c2de344746bd139dc26a40bd36b157c0df5c349d
Luca <luca.tagliavini5@studio.unibo.it>
2022-11-03 13:34:29 +0100

    allow the user to post in a board

--- api/src/handlers/game.ts
+++ api/src/handlers/game.ts
@@ -65,7 +65,7 @@ export const getScore: RequestHandler = async (req, res) => {
   let user: (IUser & { _id: ObjectId }) | null = null;
   try {
     user = await User.findOne({ _id: result.user }).exec();
-  } catch (_) {}
+  } catch (_) { }
 
   json(
     res,
@@ -89,8 +89,8 @@ export const getLeaderboard: RequestHandler = async (req, res) => {
       let user: (IUser & { _id: ObjectId }) | null = null;
       try {
         user = await User.findOne({ _id: score.user }).exec();
-      } catch (_) {}
-      return { ...score.toObject(), username: user?.username };
+      } catch (_) { }
+      return { ...score.toObject(), username: user?.username } as IGameScore;
     })
   );
 
