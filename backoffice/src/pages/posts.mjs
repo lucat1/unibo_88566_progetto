c0ad894c81cba686f0d4b11746654cdb5f75b807
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-12-02 20:17:34 +0100

    Keep working on boards and posts

---
+++ backoffice/src/pages/posts.mjs
@@ -0,0 +1,46 @@
+import { h } from "../h";
+import { Link } from "../router";
+import req from "../async";
+import fetch from "shared/fetch";
+
+const Posts = ({ board }) => {
+  const { data, loading, err } = req(`store/boards/${board}/posts`, fetch);
+  return h(
+    "main",
+    {},
+    loading
+      ? h("progress", { className: "progress is-primary" })
+      : err
+        ? h("div", { className: "notification is-danger" }, "Error: ", err)
+        : h(
+          "div",
+          { className: "menu my-4" },
+          h(
+            "div",
+            {
+              className:
+                "is-flex is-flex-direction-row is-justify-content-space-between py-2",
+            },
+            h("p", { className: "menu-label" }, "Posts"),
+            h(
+              Link,
+              { to: `/boards/${board}/add` },
+              h("button", { className: "button is-primary" }, "Add")
+            )
+          ),
+          h(
+            "ul",
+            { className: "menu-list" },
+            ...data.map((ele, i) =>
+              h(
+                "li",
+                { key: i },
+                h(Link, { to: `/posts/${ele._id}` }, ele.name)
+              )
+            )
+          )
+        )
+  );
+};
+
+export default Posts;
