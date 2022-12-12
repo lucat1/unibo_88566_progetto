c4bdda29e7c91585b04efbb28dca58cd46afc870
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-12-12 01:42:20 +0100

    Frontoffice service filters (no logic yet)

--- frontoffice/src/components/select-interval.tsx
+++ frontoffice/src/components/select-interval.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { FormEvent } from "react";
 
 export interface SelectIntervalProps {
   selected?: Date[];
@@ -9,12 +9,37 @@ const SelectInterval: React.FC<SelectIntervalProps> = ({
   selected,
   onSelect,
 }) => {
+  const handleChange1 = (event: FormEvent<HTMLFormElement>) =>
+    onSelect([event.target.value, selected[1]]);
+  const handleChange2 = (event: FormEvent<HTMLFormElement>) =>
+    onSelect([selected[0], event.target.value]);
   return (
-    <div className="field my-2">
-      <label htmlFor="category" className="label">
-        Select a time interval
-      </label>
-    </div>
+    <>
+      <div className="field my-2">
+        <label for="from" className="label">
+          From
+        </label>
+        <input
+          id="for"
+          type="datetime-local"
+          class="input"
+          value={selected ? selected[0].toString() : ""}
+          onChange={handleChange1}
+        />
+      </div>
+      <div className="field my-2">
+        <label for="to" className="label">
+          To
+        </label>
+        <input
+          id="to"
+          type="datetime-local"
+          class="input"
+          value={selected ? selected[1].toString() : ""}
+          onChange={handleChange2}
+        />
+      </div>
+    </>
   );
 };
 
