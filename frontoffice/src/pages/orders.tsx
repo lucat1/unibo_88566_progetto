969320a8829f3b6ac12b7108f70cd2feeb26437f
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2023-01-22 13:33:07 +0100

    h1 on top of Orders pages

--- frontoffice/src/pages/orders.tsx
+++ frontoffice/src/pages/orders.tsx
@@ -6,50 +6,54 @@ import Pagination from "../components/pagination";
 const Ordes: React.FC = () => {
   return (
     <>
-      <h1 className="title">Orders</h1>
-      <Pagination
-        className="table"
-        style={{ width: "100%" }}
-        ele="table"
-        child="tbody"
-        url={(page) => `store/orders?page=${page}`}
-        resource={(page) => ["orders", page]}
-        pre={
-          <thead>
-            <tr role="row">
-              <th>Index</th>
+      <main class="menu">
+        <div class="is-flex is-flex-direction-row is-justify-content-space-between py-2">
+          <h1 className="menu-label">Orders</h1>
+        </div>
+        <Pagination
+          className="table"
+          style={{ width: "100%" }}
+          ele="table"
+          child="tbody"
+          url={(page) => `store/orders?page=${page}`}
+          resource={(page) => ["orders", page]}
+          pre={
+            <thead>
+              <tr role="row">
+                <th>Index</th>
+                <th>
+                  <abbr title="Number of items">N. items</abbr>
+                </th>
+                <th>Products</th>
+                <th>Total</th>
+                <th>
+                  <abbr title="Date of purchase">Date</abbr>
+                </th>
+              </tr>
+            </thead>
+          }
+        >
+          {(ord: IOrder, i) => (
+            <tr key={i} role="row">
               <th>
-                <abbr title="Number of items">N. items</abbr>
+                <Link to={`/orders/${ord._id}`}>{i + 1}</Link>
               </th>
-              <th>Products</th>
-              <th>Total</th>
-              <th>
-                <abbr title="Date of purchase">Date</abbr>
-              </th>
+              <td>{ord.items.length}</td>
+              <td>
+                {ord.items.map((i) => (i.product || i.pet)!.name).join(", ")}
+              </td>
+              <td>
+                $
+                {ord.items.reduce(
+                  (prev, i) => prev + (i.product || i.pet)!.price * i.amount,
+                  0
+                )}
+              </td>
+              <td>{new Date(ord.date).toLocaleDateString("en-UK")}</td>
             </tr>
-          </thead>
-        }
-      >
-        {(ord: IOrder, i) => (
-          <tr key={i} role="row">
-            <th>
-              <Link to={`/orders/${ord._id}`}>{i + 1}</Link>
-            </th>
-            <td>{ord.items.length}</td>
-            <td>
-              {ord.items.map((i) => (i.product || i.pet)!.name).join(", ")}
-            </td>
-            <td>
-              $
-              {ord.items.reduce(
-                (prev, i) => prev + (i.product || i.pet)!.price * i.amount,
-                0
-              )}
-            </td>
-            <td>{new Date(ord.date).toLocaleDateString("en-UK")}</td>
-          </tr>
-        )}
-      </Pagination>
+          )}
+        </Pagination>
+      </main>
     </>
   );
 };
